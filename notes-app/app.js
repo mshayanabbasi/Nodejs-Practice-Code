34430a8bde5268dd49596dccc6a21204d7aa54aa
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-10-19 21:25:34 +0500

    Challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,10 @@
 const fs = require('fs')
-fs.writeFileSync('notes.txt', 'My name is Shayan!')
+// fs.writeFileSync('notes.txt', 'My name is Shayan!')
+
+//Challenge: Append a message to notes.txt
+// 
+// 1. Use appendFileSync to append to the file
+// 2. Run the Project
+// 3. Check your work by opening the file and viewing the append text
+
+fs.appendFileSync('notes.txt', 'I am a Full Stack Developer!')
