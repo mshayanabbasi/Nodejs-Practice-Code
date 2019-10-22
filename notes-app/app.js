74bb1e7aea42cabddc0123feeb80df66da9c99d7
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-10-22 23:21:58 +0500

    changes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,16 @@
-const validator = require('validator')
+// const validator = require('validator')
 const getNotes = require('./notes')
-
-
+const chalk = require('chalk')
 // const sum = add(4, -2)
 const notes = getNotes()
 // console.log(validator.isEmail('mshayanabbasi@gmail.com'))
-console.log(validator.isURL('https://github.com'))
+// console.log(validator.isURL('https://github.com'))
 // console.log(sum);
 console.log(notes)
+const greenMsg = chalk.green.inverse.bold('Success!')
+console.log(greenMsg);
+
+
 
 
 
