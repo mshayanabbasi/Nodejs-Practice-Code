deb591644087794c476e8425852bb6e0cdac198c
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-11-10 14:06:35 +0500

    changes

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,35 +1,35 @@
-// setTimeout(() => {
-//     console.log('two second up')
-// }, 2000)
+setTimeout(() => {
+    console.log('two second up')
+}, 2000)
 
-// const names = ['Shayan', 'Ashar', 'Ahsan']
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4
-// })
+const names = ['Shayan', 'Ashar', 'Ahsan']
+const shortNames = names.filter((name) => {
+    return name.length <= 4
+})
 
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-//         callback(data)
-//     }, 2000)
-// }
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        }
+        callback(data)
+    }, 2000)
+}
 
-// geocode('California', (data) => {
-//     console.log(data)
-// })
+geocode('California', (data) => {
+    console.log(data)
+})
 
-// const add = (num1, num2, callback) => {
-//     setTimeout(() => {
-//         const sum = num1 + num2
-//         callback(sum)
-//     },2000) 
-// }
-// add(1, 4, (sum) => {
-//     console.log(sum)
-// })
+const add = (num1, num2, callback) => {
+    setTimeout(() => {
+        const sum = num1 + num2
+        callback(sum)
+    },2000) 
+}
+add(1, 4, (sum) => {
+    console.log(sum)
+})
 const dpWorkCallback = (callback) => {
     setTimeout(() => {
         // callback('This is my error!', undefined)
