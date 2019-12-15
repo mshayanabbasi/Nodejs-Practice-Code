8fd1c26546a856b4883aef984aac760b5e240f24
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-12-15 20:12:32 +0500

    changes

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,25 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
     useNewUrlParser: true
 })
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task = new Task({
-    description: 'Clean the house',
-    completed: true
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch(() => {
-    console.log('Error', error)
-})
